ebed66ad81b7965c25be2dbc9ad8b8bc50fbf8e6
DayvisParedes <DayvisParedes@gmail.com>
2025-07-20 21:45:21 -0500

    Terminada la clase (ListaComputadora)

--- WinFormsApp2/LisComputadora.cs
+++ WinFormsApp2/LisComputadora.cs
@@ -4,9 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinFormsApp2
+namespace ComputadorasWF
 {
-    internal class LisComputadora
+    public class ListComputadoras
     {
+        public List<Computadora> Lista { get; set; }
+        public ListComputadoras()
+        {
+            Lista = new List<Computadora>();
+        }
+        public void Agregar(Computadora computadora)
+        {
+            Lista.Add(computadora);
+        }
+
+        public List<Computadora> ModeloEmpiezaL(string cadena)
+        {
+            List<Computadora> listaAux = new List<Computadora>();
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].Modelo.StartsWith(cadena))
+                {
+                    listaAux.Add(Lista[i]);
+                }
+            }
+            return listaAux;
+        }
+
+        public List<Computadora> AnioIgual(string cadena)
+        {
+            List<Computadora> listAux2 = new List<Computadora>();
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].Anio.Equals(cadena))
+                {
+                    listAux2.Add(Lista[i]);
+                }
+            }
+            return listAux2;
+        }
+
+        public int RamCifras()
+        {
+            int aux = 0;
+            foreach (Computadora computadora in Lista)
+            {
+                if (computadora.MemoriaR >= 10)
+                {
+                    aux++;
+                }
+            }
+            return aux;
+        }
+
     }
 }
